c18f44f8ed1f5e999e596748f5b958baabfce70d
yann <yannlefranc330@gmail.com>
2023-04-05 13:25:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        //utilisation de l'opérateur ou
+
+        System.out.println(x > 3 || x < 4);
+
+        
+
+    }
+}
